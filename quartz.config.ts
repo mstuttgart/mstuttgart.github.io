74b9a5e03d72341e86fcead65e4f77924983c4f7
Michell Stuttgart <michellstut@gmail.com>
2024-07-16 21:03:44 -0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,11 +8,12 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Wiki",
+    pageTitle: "Hey! It's MStuttgart's Digital Garden 🌱",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-    //   provider: "plausible",
+      //provider: "google",
+      //tagId: "G-FD48XZZQHK"
     },
     locale: "en-US",
     baseUrl: "mstuttgart.github.io/quartz",
@@ -56,19 +57,19 @@ const config: QuartzConfig = {
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "filesystem"],
       }),
-      Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting({
         theme: {
-          light: "one-dark-pro",
-          dark: "one-dark-pro",
+          light: "github-light",
+          dark: "github-dark",
         },
-        keepBackground: true,
+        keepBackground: false,
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
